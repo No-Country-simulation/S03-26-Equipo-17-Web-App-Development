5e55f690539a08f4df303c502bee99281f592c6d
An7h0ny1 <91570972+An7h0ny1@users.noreply.github.com>
2026-04-06 19:24:45 -0500

    fix:whatsapp incoming

--- backend/src/main/java/nocountry/crm/shared/config/SecurityConfig.java
+++ backend/src/main/java/nocountry/crm/shared/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
             .cors(cors -> cors.configure(http))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/v1/auth/**").permitAll()
+                    .requestMatchers("/api/v1/whatsapp/webhook").permitAll()
                 .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll() // Para Swagger
                 .anyRequest().authenticated()
             )
